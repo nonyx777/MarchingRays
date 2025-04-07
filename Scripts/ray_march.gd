30ea9431d120da5889892f8c3b349a9bc98e030a
nonyx777 <nahomretebo@gmail.com>
2025-04-07 17:08:55 +0300

    ability to apply union operator on three primitives

--- Scripts/ray_march.gd
+++ Scripts/ray_march.gd
@@ -1,16 +1,18 @@
 extends Node3D
-@onready var quad: MeshInstance3D = $MeshInstance3D
+@onready var quad: MeshInstance3D = $Quad
 
 var material: Material
 @export_range(0.0, 1.0, 0.01, "slider") var smoothness: float = 0.5
-@export var sphere_position: Vector3 = Vector3(0., 0.2, -5);
-@export var box_position: Vector3 = Vector3(0., -0.2, -5);
+@export var sphere_position: Vector3 = Vector3(0.0, 0.0, -2.0)
+@export var box_position: Vector3 = Vector3(0.0, 0.0, -2.0)
+@export var torus_position: Vector3 = Vector3(0.0, 0.0, -2.0)
 @export_range(-1.0, 1.0, 0.01, "slider") var sphere_ypos: float = 0.0;
 
 func _ready() -> void:
 	material = quad.mesh.surface_get_material(0)
 	material.set_shader_parameter("sphere_position", sphere_position)
 	material.set_shader_parameter("box_position", box_position)
+	material.set_shader_parameter("torus_position", torus_position)
 func _process(delta: float) -> void:
 	material.set_shader_parameter("smoothness", smoothness)
-	material.set_shader_parameter("sphere_ypos", sphere_ypos)
+	#material.set_shader_parameter("sphere_ypos", sphere_ypos)
